4abf59f9166f16285854d30cca1ef1a0b8610688
widjesh <widjesh@satyamholidays.com>
2019-10-18 21:04:52 -0500

    Updated Controller

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+var ObjectId = require("mongoose").Types.ObjectId;
+var User = require('../models/user');
 
 
 module.exports = router;
