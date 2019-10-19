3f76da72f1cc258aade7e502b9cc6e1e1a33e5a1
widjesh <widjesh@satyamholidays.com>
2019-10-18 21:31:35 -0500

    Fix Async/Await

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,36 +1,31 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 var ObjectId = require("mongoose").Types.ObjectId;
-var User = require('../models/user');
+var User = require("../models/user");
 
-router.get('/users', (req, res, next) => {
-    User.find((err, users) => {
-        if (!err) {
-            if (users) res.json(users)
-            else res.send('No Users Found')
-        } else {
-            console.error(err)
-        }
-    })
-})
-
-router.get('/users/:email', (req, res, next) => {
-    const email = req.params.email
-    User.findOne({ email: email }, (err, user) => {
-        if (!err) {
-            if (user) res.json(user)
-            else res.send(`User with id ${email} not found`)
-        } else {
-            console.error(err)
-        }
-    })
-})
-
-router.get('/:email', async (req, res) => {
-    const user = await User.findOne({ email: req.params.email });
-    res.send(user);
+router.get("/", (req, res, next) => {
+  User.find((err, users) => {
+    if (!err) {
+      if (users) res.json(users);
+      else res.send("No Users Found");
+    } else {
+      console.error(err);
+    }
+  });
 });
 
+router.get("/:email", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email });
+    if (!user) {
+      res.send(`${req.params.email} not found`);
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.send({ err });
+  }
+});
 
 module.exports = router;
