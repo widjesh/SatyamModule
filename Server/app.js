8518edaae7c047fcc576f41e0dac0dbee3327ffe
widjesh <widjesh@satyamholidays.com>
2019-10-18 21:19:27 -0500

    Implemented Routes

--- Server/app.js
+++ Server/app.js
@@ -8,6 +8,7 @@ const {mongoose} = require('./database/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const users = require('./controllers/userController');
 
 var app = express();
 
@@ -22,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users',users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 var ObjectId = require("mongoose").Types.ObjectId;
 var User = require('../models/user');
 
+router.get('/:email',async (req,res)=>{
+    const user = await User.findOne({email:req.params.email});
+    res.send(user);
+});
 
 module.exports = router;
