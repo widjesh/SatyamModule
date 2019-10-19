43d3182cf7cebce9afcaea1d1052aab450bb6b6f
KalebWorkneh <kalebzenawi@gmail.com>
2019-10-18 21:23:50 -0500

    Update controller

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,9 +1,37 @@
 const router = require('express').Router();
-const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 var ObjectId = require("mongoose").Types.ObjectId;
 var User = require('../models/user');
 
+<<<<<<< HEAD
+router.get('/users', (req, res, next) => {
+    User.find((err, users) => {
+        if (!err) {
+            if (users) res.json(users)
+            else res.send('No Users Found')
+        } else {
+            console.error(err)
+        }
+    })
+})
+
+router.get('/users/:email', (req, res, next) => {
+    const email = req.params.email
+    User.findOne({ email: email }, (err, user) => {
+        if (!err) {
+            if (user) res.json(user)
+            else res.send(`User with id ${email} not found`)
+        } else {
+            console.error(err)
+        }
+    })
+})
+=======
+router.get('/:email',async (req,res)=>{
+    const user = await User.findOne({email:req.params.email});
+    res.send(user);
+});
+>>>>>>> 8518edaae7c047fcc576f41e0dac0dbee3327ffe
 
 module.exports = router;
