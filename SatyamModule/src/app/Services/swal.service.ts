d5fec545a7dec21066c11ac744f8f33d07aa03cf
stmamo <stmamo@mum.edu>
2019-10-19 19:01:32 -0500

    navigation

--- SatyamModule/src/app/Services/swal.service.ts
+++ SatyamModule/src/app/Services/swal.service.ts
@@ -8,7 +8,8 @@ export class SwalService {
 
   constructor() { }
 
-  notify(s1:string,s2:string,s3:string){
-    Swal(s1,s2,s3);
+  notify(s1: string, s2: string, s3: string) {
+    Swal(s1, s2, s3);
+    //check
   }
 }
